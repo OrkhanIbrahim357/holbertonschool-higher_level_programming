8700f929786517b27c4a681945252e58342700b6
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-09-15 12:59:35 +0400

    changed

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary.keys():
-        del a_dictionary[key]
-    return a_dictionary
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for k, v in a_dictionary.items():
+        new_dict[k] = v * 2
+    return new_dict
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
