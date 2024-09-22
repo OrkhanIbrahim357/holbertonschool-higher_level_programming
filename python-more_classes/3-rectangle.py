69116c9e54068e3570ba7c5beacc3f4415fd9eee
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-09-22 16:54:39 +0400

    tapdim

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return (self.width + self.height) * 2
-    
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ''
