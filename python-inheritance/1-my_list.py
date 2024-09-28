c389ce874f44ad445d116d25d245972327c34fc5
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-09-28 19:50:44 +0400

    zehraaaaa

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module containing print_sorted func"""
 
+
 class MyList(list):
     """Class inherited from list class"""
 
@@ -8,4 +9,3 @@ class MyList(list):
         """Method to print sorted list"""
         new_list = sorted(self)
         print(new_list)
-        
