ad9270244f6e8d7c3a1d6e2387c99dda01b1694a
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-10-03 02:04:30 +0400

    saalam

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ def write_file(filename="", text=""):
     """The write_file function"""
 
     # Opening and writing to the file
+
     with open (filename, 'w', encoding='UTF-8') as f:
         return f.write(text)
