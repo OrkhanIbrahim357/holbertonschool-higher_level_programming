68e096a2812f54a8ee64d0c47d72875cde0e134a
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-10-03 03:48:55 +0400

    salam

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,7 +6,7 @@ def load_from_json_file(filename):
     """Function to create Object from JSON file"""
 
     # Opening and reading the file
-    with open(filename 'r', encoding='UTF-8') as file:
+    with open(filename,'r', encoding='UTF-8') as file:
         read_data = file.read()
 
     # Making file object and returning
